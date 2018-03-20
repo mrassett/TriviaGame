aa472c5cce770f3c1f25a0a6dc354eae31736a71
mrassett <magsrassett@gmail.com>
2018-03-20 18:13:21 -0500

    final game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,69 +1,66 @@
-//append html elements to the DOM 
-//when start button is clicked, the page should be updated with timer & quiz questions.
-//wrap the timer fuction in the click event "on.click - start time,"
-//use jquery to delete html elements & add html elements
-//h1, div holding button, acts as a container for all elements moving forward
-//targeting that containing element. Use .append or .prepend, inside of the append function 
-//create the div element inside of the append element, it should be dynamic
-//.remove use to delete existing button, when delete button you inject the other elements that are needed
-//container, container, elements (for the second slide)
-//iterate over array, "for each questions " append an element to the DOM containing the questions and answers
-var questionsArray =  [" ","How are you?", "Fav Color?"];
-
-let answerArray = [["fine", "well", "good","shitty"], 
-                    ["blue","black","gold","gray"]];
-
-let correctAnswers =["well","gold"];
+"use strict"
 let timer; 
-let timeLeft = 20;
+let timeLeft = 30;
 let questionNumber = 0;
-let answered = "";
 let questionUserisOn = 0;
-//start button gets clicked, new page pops up with questions
-//write new div with 3 questions
-//timer starts
-//function to count timer down starts
+let numberofCorrectAnswers = 0;
 
-
-//Start button
-$(document).ready(function(){    
-    let startButton =  $('#start').on('click',function() {
-    $("#start").hide();
-    showQuestions();
-    })
+$(document).ready(function(){
+    $("#myform").hide();
+    $("#right").hide();
+    $("#wrong").hide();
+    $("#answerpage").hide();
 })
 
-//New question & answer slide
-function showQuestions() {
-    $('#right-wrong').empty();
-    answered = true;
-    var triviaBox = $("#questionbox").html(`<h2>${questionsArray[+1]}<br></h2>
-    <button type="button" id="answer-button">${answerArray[questionUserisOn][0]}</button><br>
-    <button type="button" id="answer-button">${answerArray[questionUserisOn][1]}</button><br>
-    <button type="button" id="answer-button">${answerArray[questionUserisOn][2]}</button><br>
-    <button type="button" id="answer-button">${answerArray[questionUserisOn][3]}</button><br>`)
-    countDownBegins ();
-    };
-//timer function
-    function countDownBegins (){
-        timer = setInterval(function(){
-        $("#countdown").html(timeLeft);
-        if (timeLeft === 0){
-        clearInterval(timer);
-       } else{
-        timeLeft--;
-        }
-        }, 2000);
-        console.log(countDownBegins);
+let startButton =  $('#start').on('click',function startsUp() {
+    $("#start").hide();
+    $("#myform").show();
+    countDownBegins();
+});
+let submitButton =  $('#submit').on('click',function(event) {
+    event.preventDefault();
+    $("#start").hide();
+    $("#myform").hide();
+    $("#answerpage").show();
+    $("#right").show();
+    $("#right").html(numberofCorrectAnswers);
+    
+});
+
+//function that displays questions
+function check(){
+let question1 = $("input[name='quizquestion']:checked").val();
+if (question1 == "correct") {numberofCorrectAnswers++} 
+let question2 = $("input[name='quizquestion2']:checked").val();
+if (question2 == "correct") {numberofCorrectAnswers++} 
+let question3 = $("input[name='quizquestion3']:checked").val();
+if (question3 == "correct") {numberofCorrectAnswers++} 
+let question4 = $("input[name='quizquestion4']:checked").val();
+if (question4 == "correct") {numberofCorrectAnswers++} 
+let question5 = $("input[name='quizquestion5']:checked").val();
+if (question5 == "correct") {numberofCorrectAnswers++} 
+
+console.log(question1,question2,question3,question4,question5);
+console.log(numberofCorrectAnswers);
+}
+
+function countDownBegins (){
+    timer = setInterval(function(){
+    $("#countdown").html(timeLeft);
+    if (timeLeft === 0){
+    timeout();
+    clearInterval(timer);
+   } else{
+    timeLeft--;
     }
-        
-
-// for (let i = 0; i <= questionsArray.length; i++) {
-//     let question = questionsArray[i].question
-//     let someting = questionsArray[i].answers
-//     $('#questionbox').append(`<div>${question}</div>`)
-
-// person = { name: "Bob", gender: "?"}
-
-//var personsName = person.name
+    }, 1000);
+    console.log(countDownBegins);
+}
+function timeout(){
+        $("#start").hide();
+        $("#myform").hide();
+        $("#right").show();
+        $("#right").html(numberofCorrectAnswers);
+        $("#answerpage").show();
+    }
 
